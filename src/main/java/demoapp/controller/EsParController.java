38075a6d7f6cebec50b8a7381249c52fe7b295df
ElenaMegia <emegican@myuax.com>
2022-10-12 00:35:54 +0200

    EsParController 1.0

--- src/main/java/demoapp/controller/EsParController.java
+++ src/main/java/demoapp/controller/EsParController.java
@@ -1,4 +1,23 @@
 package demoapp.controller;
 
+import demoapp.service.NumeroParService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class EsParController {
+
+    @Autowired
+    private NumeroParService service;
+
+    @RequestMapping("/esNumeroPar")
+    
+
+
+
+
+
 }
