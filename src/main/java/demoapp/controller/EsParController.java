1e51744ed8f3b753f769829d2adbe1de2e25a186
ElenaMegia <emegican@myuax.com>
2022-10-12 11:04:19 +0200

    EsParController 1.1

--- src/main/java/demoapp/controller/EsParController.java
+++ src/main/java/demoapp/controller/EsParController.java
@@ -4,8 +4,11 @@ import demoapp.service.NumeroParService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class EsParController {
@@ -13,8 +16,27 @@ public class EsParController {
     @Autowired
     private NumeroParService service;
 
-    @RequestMapping("/esNumeroPar")
-    
+
+    //Se declara con NumeroData
+    @GetMapping("/esPar")
+    public String saludoForm(NumeroData numeroData){
+        return "formGetNumero";
+    }
+
+    @PostMapping("/esPar")
+    public String esPar(@ModelAttribute @Valid NumeroData numero, BindingResult bindingResult, Model model){
+       String respuesta;
+        if(bindingResult.hasErrors()){
+            respuesta= "formGetNumero";
+        } else{
+            model.addAttribute("numero, numero.getNumero()");
+            model.addAttribute("esPar", service.esPar(numero.getNumero()));
+            respuesta="esParComprobado";
+        }
+        return respuesta;
+    }
+
+
 
 
 
